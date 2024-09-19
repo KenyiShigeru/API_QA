d283692e40dc5a981d908ed44bd33f3c655eb948
Kenyi Shigeru Cervantes Creano <kidvskat_kenyi@hotmail.com>
2024-09-19 11:53:35 -0700

    Archivo de conexión creado correctamente

--- .env/conexion.js
+++ .env/conexion.js
@@ -1,6 +1,4 @@
-import mysql from 'mysql';
-
-const connection = mysql.createConnection();
+const mysql = require('mysql');
 
 datosConexion = 
 {
@@ -9,3 +7,15 @@ datosConexion =
     password: '',
     database: 'qualityart'
 };
+
+const connection = mysql.createConnection(datosConexion);
+
+function conectar(error){
+    if(error){
+        console.error('Error al conectar a la base de datos',error);
+        return;
+    }
+    console.log('Conexion a la base de datos creada correctamente');
+}
+
+connection.connect(conectar);
