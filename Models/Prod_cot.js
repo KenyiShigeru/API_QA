cd031534e4bdd4a41d5d3842da40b2409c277a5b
KenyiShigeru <kidvskat_kenyi@hotmail.com>
2024-11-22 17:00:58 -0700

    Correccion de bug de un solo producto por cotizacion

--- Models/Prod_cot.js
+++ Models/Prod_cot.js
@@ -7,7 +7,7 @@ class Prod_cotModel {
         return new Promise((resolve, reject) => {
             this.conexion.execute('select * from prod_cotizacion', (error, resultados) => {
                 if (error) return reject(error);
-                resolve(resultados[0]);
+                resolve(resultados);
             });
         });
     }
