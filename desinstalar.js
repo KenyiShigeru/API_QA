14857b183a9a054f113c63f20ded82b96a27dff7
Kenyi Shigeru Cervantes Creano <kidvskat_kenyi@hotmail.com>
2024-11-28 10:30:33 -0700

    desinstalar servicio

---
+++ desinstalar.js
@@ -0,0 +1,25 @@
+//Este script se encarga de instalar el servicio en windows
+//Para usarlo debo instalar node-windows con npm de manera global
+
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'API_QA',
+  description: 'API de backend para el software de Quality Art.',
+  script: 'D:\\Residencias\\API_QA\\index.js',
+  nodeOptions: [
+    '--harmony',
+    '--max_old_space_size=4096'
+  ]
+  //, workingDirectory: '...'
+  //, allowServiceLogon: true
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('uninstall',function(){
+  svc.start();
+});
+
+svc.uninstall();
